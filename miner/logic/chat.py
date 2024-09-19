942a777a2992c7a12ab9ccdc5857eed132baf48b
sirouk <8901571+sirouk@users.noreply.github.com>
2024-09-19 17:32:13 -0400

    get task name for mapping and fallback to default if unmapped

--- miner/logic/chat.py
+++ miner/logic/chat.py
@@ -17,21 +17,22 @@ async def chat_stream(
     if task_config is None:
         raise ValueError(f"Task config not found for model: {decrypted_payload.model}")
     assert task_config.orchestrator_server_config.load_model_config is not None
-    print(f"Chat task config: {task_config}")
+    #print(f"Chat task config: {task_config}")
 
-    model_name = task_config.orchestrator_server_config.load_model_config["model"]
-    decrypted_payload.model = model_name
-
-    if decrypted_payload.model == "unsloth/Meta-Llama-3.1-8B-Instruct":
-        address = worker_config.LLAMA_3_1_8B_TEXT_WORKER_URL
-    elif decrypted_payload.model == "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4":
-        address = worker_config.LLAMA_3_1_70B_TEXT_WORKER_URL
-    else:
-        raise ValueError(f"Invalid model: {decrypted_payload.model}")
+    address = None
+    address, _ = map_endpoint_with_override(address, task_config.orchestrator_server_config.task, address)
+    if address is None:
+        model_name = task_config.orchestrator_server_config.load_model_config["model"]
+        #decrypted_payload.model = model_name
+        if model_name == "unsloth/Meta-Llama-3.1-8B-Instruct":
+            address = worker_config.LLAMA_3_1_8B_TEXT_WORKER_URL
+        elif model_name == "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4":
+            address = worker_config.LLAMA_3_1_70B_TEXT_WORKER_URL
+        else:
+            raise ValueError(f"Invalid model: {decrypted_payload.model}")
 
     assert address is not None, f"Address for model: {decrypted_payload.model} is not set in env vars!"
 
-    address, _ = map_endpoint_with_override(address, task_config.task, address)
 
     async with aiohttp_client.post(address, json=decrypted_payload.model_dump(), timeout=3) as resp:
         resp.raise_for_status()
