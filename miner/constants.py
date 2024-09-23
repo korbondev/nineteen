031143c84977f420e0a27b3171ebb8e7d746ecde
Your Name <you@example.com>
2024-09-23 04:36:11 +0000

    reorder endpoint maps to match our setup

--- miner/constants.py
+++ miner/constants.py
@@ -6,16 +6,16 @@ AVATAR_SERVER_ENDPOINT = "avatar"
 
 ENDPOINT_TO_PORT_MAP = {
     # endpoint in map should have no leading / 
+    "chat-llama-3-1-70b": (7405, "generate", None), # need to check vllm endpoint route
+    "chat-llama-3-1-8b": (7105, "generate", None), # need to check vllm endpoint route
     "avatar": (7212, "avatar", "avatar"),
     "inpaint": (7222, "inpaint", "inpaint"),
     "proteus-text-to-image": (7231, "txt2img", "proteus"),
-    "proteus-image-to-image": (7232, "img2img", "proteus"),
-    "dreamshaper-text-to-image": (7241, "txt2img", "dreamshaper"),
-    "dreamshaper-image-to-image": (7242, "img2img", "dreamshaper"),
     "flux-schnell-text-to-image": (7251, "txt2img", "flux-schnell"),
+    "dreamshaper-text-to-image": (7241, "txt2img", "dreamshaper"),
+    "proteus-image-to-image": (7232, "img2img", "proteus"),
     "flux-schnell-image-to-image": (7252, "img2img", "flux-schnell"),
-    "chat-llama-3-1-70b": (7405, "generate", None), # need to check vllm endpoint route
-    "chat-llama-3-1-8b": (7105, "generate", None), # need to check vllm endpoint route
+    "dreamshaper-image-to-image": (7242, "img2img", "dreamshaper"),
 }
 
 def map_endpoint_with_override(post_endpoint, task, default_endpoint):
