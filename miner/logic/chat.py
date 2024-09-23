1f85c5a007e3e0377030ef0d3cc5dae144cae3dd
sirouk <8901571+sirouk@users.noreply.github.com>
2024-09-23 15:47:15 -0400

    fix for model name

--- miner/logic/chat.py
+++ miner/logic/chat.py
@@ -19,22 +19,24 @@ async def chat_stream(
     
     logger.info(f"in chat_stream() decrypted_payload.model: {decrypted_payload.model}")
     
+
     task_config = tcfg.get_enabled_task_config(decrypted_payload.model)
     if task_config is None:
         raise ValueError(f"Task config not found for model: {decrypted_payload.model}")
     assert task_config.orchestrator_server_config.load_model_config is not None
+    model_name = task_config.orchestrator_server_config.load_model_config["model"]
+    
 
-    decrypted_payload.model = task_config.orchestrator_server_config.load_model_config["model"]
-    if task_config.orchestrator_server_config.load_model_config["model"] == "unsloth/Meta-Llama-3.1-8B-Instruct":
+    if task_config.task == Task.chat_llama_3_1_8b:
         address = worker_config.LLAMA_3_1_8B_TEXT_WORKER_URL
-    elif task_config.orchestrator_server_config.load_model_config["model"] == "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4":
+    elif task_config.task == Task.chat_llama_3_1_70b:
         address = worker_config.LLAMA_3_1_70B_TEXT_WORKER_URL
-    elif task_config.orchestrator_server_config.load_model_config["model"] == "mattshumer/reflection-70b":
-        address = worker_config.REFLECTION_70B_TEXT_WORKER_URL
     else:
         raise ValueError(f"Invalid model: {decrypted_payload.model}")
+    
+    decrypted_payload.model = model_name
 
-    address, _ = map_endpoint_with_override(None, decrypted_payload.model, None)
+    address, _ = map_endpoint_with_override(None, task_config.task, None)
     
     assert address is not None, f"Address for model: {decrypted_payload.model} is not set in env vars!"
 
