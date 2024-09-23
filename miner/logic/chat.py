1f9db1e8efd15fb3adce75a8aa95996dcc2d44f0
i-beck <99846875+i-beck@users.noreply.github.com>
2024-09-23 16:18:03 -0400

    cleaned up async generator code to stop peaking at first chunk to make sure there is data, just raise a 500 error

--- miner/logic/chat.py
+++ miner/logic/chat.py
@@ -1,10 +1,11 @@
 import aiohttp
 import traceback
 from fiber.logging_utils import get_logger
-
+from fastapi import HTTPException
 from core.models import payload_models
 from core import tasks_config as tcfg
-from core.tasks import Task
+
+# from core.tasks import Task
 from miner.config import WorkerConfig
 from miner.constants import map_endpoint_with_override
 
@@ -14,14 +15,26 @@ logger = get_logger(__name__)
 
 
 async def chat_stream(
-    aiohttp_client: aiohttp.ClientSession, decrypted_payload: payload_models.ChatPayload, worker_config: WorkerConfig
-) -> AsyncGenerator[str | None, Any]:
-    
+    aiohttp_client: aiohttp.ClientSession,
+    decrypted_payload: payload_models.ChatPayload,
+    worker_config: WorkerConfig,
+    client_timeout: float = 5.0,
+) -> AsyncGenerator[str, Any]:
+    """
+    Stream the chat interaction with the chat server.
+
+    Args:
+        aiohttp_client (aiohttp.ClientSession): The aiohttp client to use for sending the request.
+        decrypted_payload (payload_models.ChatPayload): The decrypted chat payload to send to the server.
+        worker_config (WorkerConfig): The configuration of the worker service.
+        client_timeout (float, optional): The timeout for the client in seconds. Defaults to 5.0.
+
+    Yields:
+        str: The response as a stream of SSE messages.
+    """
+    # Determine the address of the chat server to send the request to
     logger.info(f"in chat_stream() decrypted_payload.model: {decrypted_payload.model}")
-    logger.error(f"in chat_stream() decrypted_payload.model: {decrypted_payload.model}")
-    print(f"in chat_stream() decrypted_payload.model: {decrypted_payload.model}")
-    #quit()
-    
+
     address, _ = map_endpoint_with_override(None, decrypted_payload.model, None)
     if address is None:
         task_config = tcfg.get_enabled_task_config(decrypted_payload.model)
@@ -29,10 +42,13 @@ async def chat_stream(
             raise ValueError(f"Task config not found for model: {decrypted_payload.model}")
         assert task_config.orchestrator_server_config.load_model_config is not None
 
-        #decrypted_payload.model = task_config.orchestrator_server_config.load_model_config["model"]
+        # decrypted_payload.model = task_config.orchestrator_server_config.load_model_config["model"]
         if task_config.orchestrator_server_config.load_model_config["model"] == "unsloth/Meta-Llama-3.1-8B-Instruct":
             address = worker_config.LLAMA_3_1_8B_TEXT_WORKER_URL
-        elif task_config.orchestrator_server_config.load_model_config["model"] == "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4":
+        elif (
+            task_config.orchestrator_server_config.load_model_config["model"]
+            == "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4"
+        ):
             address = worker_config.LLAMA_3_1_70B_TEXT_WORKER_URL
         elif task_config.orchestrator_server_config.load_model_config["model"] == "mattshumer/reflection-70b":
             address = worker_config.REFLECTION_70B_TEXT_WORKER_URL
@@ -43,12 +59,15 @@ async def chat_stream(
 
     logger.info(f"Sending request to {address}")
 
-    timeout = aiohttp.ClientTimeout(total=5)
+    # Send the request to the chat server
+    timeout = aiohttp.ClientTimeout(total=client_timeout)
     async with aiohttp_client.post(address, json=decrypted_payload.model_dump(), raise_for_status=True, timeout=timeout) as resp:
         if resp.status != 200:
-            logger.error(f"Error in streaming text from the server: {resp.status}.")
-            yield None
+            logger.error(f"(Not200) Error in streaming text from the server: {resp.status}.")
+            raise HTTPException(status_code=500, detail=f"Error in streaming text from the server. StatCode({resp.status})")
 
+        # Yield the response as a stream of SSE messages
+        chunk = True
         async for chunk_enc in resp.content:
             chunk = None
             try:
@@ -62,4 +81,11 @@ async def chat_stream(
                     yield f"data: {data}\n\n"
             except Exception as e:
                 logger.error(f"Error in streaming text from the server: {e}. Original chunk: {chunk}\n{traceback.format_exc()}")
-                yield None
+                raise HTTPException(status_code=500, detail="Error in streaming text from the server") from e
+            chunk = None
+
+        if chunk is not None:
+            logger.error("Error in streaming text from the server: resp.content contains no chunks.")
+            raise HTTPException(status_code=500, detail="Error in streaming text from the server (resp.content empty)")
+
+    yield ""
