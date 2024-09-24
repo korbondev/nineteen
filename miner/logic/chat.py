a6ab3ffac89f2682e247e3c41944549f4afa896e
sirouk <8901571+sirouk@users.noreply.github.com>
2024-09-24 15:55:08 -0400

    add retries to chat connectivity

--- miner/logic/chat.py
+++ miner/logic/chat.py
@@ -1,6 +1,7 @@
 import time
 import aiohttp
-import traceback
+from aiohttp import ClientConnectorError
+import asyncio
 from fiber.logging_utils import get_logger
 
 from core.models import payload_models
@@ -38,25 +39,45 @@ async def chat_stream(
 
     logger.info(f"in chat_stream() task: {task_config.task}")
 
-    count = 0
     started_at = time.time()
     timeout = aiohttp.ClientTimeout(total=30)
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.post(address, json=decrypted_payload.model_dump(), timeout=timeout) as resp:
-                resp.raise_for_status()
+    count = 0
 
-                async for chunk_enc in resp.content:
-                    if chunk := chunk_enc.decode():
-                        yield chunk
-                        if 'data:' in chunk:
-                            count += 1
-        except Exception as e:
-            logger.error(
-                f"Error in streaming text from the server {e}"
-            )
-            raise
+    max_retries = 3
 
-        finally:
-            delta = time.time() - started_at
-            logger.info(f"task: {task_config.task} streamed {count} tokens in {round(delta, 4)} seconds @ {round(count / delta, 6)} tps")
+    for retries in range(1, max_retries + 1):
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.post(address, json=decrypted_payload.model_dump(), timeout=timeout) as response:
+                    # retry on 500 error
+                    if 500 <= response.status < 600:
+                        logger.warning(f"Attempt {retries}: Received {response.status} error. Retrying...")
+                        continue
+                    response.raise_for_status()
+
+                    async for chunk_enc in response.content:
+                        if chunk := chunk_enc.decode():
+                            yield chunk
+                            if 'data:' in chunk:
+                                count += 1
+
+                    delta = time.time() - started_at
+                    logger.info(f"task: {task_config.task} streamed {count} tokens in {round(delta, 4)} seconds @ {round(count / delta, 6)} tps")
+                    return
+                
+            # retry on connection error
+            except (ClientConnectorError, asyncio.TimeoutError) as e:
+                logger.warning(f"Attempt {retries}: Connection error {e}. Retrying...")
+                continue
+
+            except Exception as e:
+                logger.error(
+                    f"Error in streaming text from the server {e}"
+                )
+                raise
+
+
+    # Exhausted all retries
+    delta = time.time() - started_at
+    logger.error(f"task: {task_config.task} retried {max_retries} times and failed in {round(delta, 4)} seconds")
+    raise
